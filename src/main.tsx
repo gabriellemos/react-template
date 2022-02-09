3cf821b047e95d5fd031fa5c98b0da09825f0f59
Gabriel Lemos <gabrieldebritolemos@gmail.com>
2022-02-09 09:29:06 -0300

    chore: updated project to recognize absolute paths while importing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './css/normalize.css';
-import './css/index.css';
+import 'css/normalize.css';
+import 'css/index.css';
 
-import App from './App';
+import App from 'App';
 
 ReactDOM.render(
   <React.StrictMode>
